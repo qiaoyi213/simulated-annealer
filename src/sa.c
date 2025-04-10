ea1f528e954ec2f3bb0eb42f824e95d23679a7fb
qiaoyi213 <linqiaoyi213@gmail.com>
2025-04-11 00:22:49 +0800

    feat: implement sa

--- src/sa.c
+++ src/sa.c
@@ -1,4 +1,5 @@
 #include "matrix.h"
+#include<math.h>
 double calculate_hamiltonain(matrix_t Q, matrix_t x){
     matrix_t x_t = matrix_transpose(x);
     matrix_t m = matrix_mul(x_t, Q);
@@ -6,4 +7,34 @@ double calculate_hamiltonain(matrix_t Q, matrix_t x){
     return result.elements[0];
 }
 
+double sa_sample(matrix_t Q) {
+    matrix_t s = matrix_create(Q.row);
+    int k_max = 100;
 
+    for(int k=0;k<k_max;k++) {
+        T = temperature(k, k_max);
+        matrix_t sn = neighbour(s);
+        double e = calculate_hamiltonain(Q, s);
+        double en = calculate_hamiltonain(Q, sn);
+
+        if(rand% 100 < int(P(e, en, T))){
+            s = sn;
+            e = en;
+        }
+    }
+    return s;
+}
+double temperature(int k, int k_max){
+     
+    return 1-((k+1)/k_max);
+}
+double P(double e, double en, double T) {
+    if(en > e) return 1;
+    return exp((e - en)/T);
+}
+matrix_t neighbour(matrix_t x) {
+    int len = x.rows;
+    int pos = rand() % len;
+    x.elements[0][pos] = -1 * (x.elements[0][pos] -1);
+    return x;
+}
